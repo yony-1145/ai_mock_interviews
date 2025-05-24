cf08831e718d2fddc658fa6e7d2ba3ecaf3bfe09
yony-1145 <yo.hm1515@icloud.com>
2025-05-24 20:42:03 +0900

    feat: add a core feature section on home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,10 +3,9 @@ import Image from 'next/image';
 
 export default function HomePage() {
   return (
-    <main className="min-h-screen flex flex-col">
-      <section className="flex flex-col items-center justify-between flex-1 px-6 lg:px-24 py-16">
-
-        <div className="flex-1 mt-12 lg:mt-0 flex justify-center"> 
+    <main className="min-h-screen flex flex-col space-y-12">
+      <section className="px-6 lg:px-24 py-4">
+        <div className="mt-12 lg:mt-0 flex justify-center"> 
           <Image
             src="/demo.png"
             alt="App Image"
@@ -15,9 +14,10 @@ export default function HomePage() {
           />
         </div>
 
-        <div className="flex-1 mt-12 space-y-6 text-center">
+        <div className="mt-12 space-y-6 text-center">
           <h1 className="text-5xl font-extrabold leading-tight">
-            Get Your Own Interview with AI-powerd
+            Get Your Own Interview <br/>
+            with AI-powerd
           </h1>
           <Link
             href="/sign-up"
@@ -26,8 +26,72 @@ export default function HomePage() {
             Get Started
           </Link>
         </div>
-
       </section>
+
+      <section className="space-y-16 px-6 lg:px-24 py-4">
+        <div>
+          <h2 className="text-center text-5xl font-extrabold leading-tight">
+            Core Features
+          </h2>
+        </div>
+
+        <div className='flex flex-row justify-between space-x-8 mt-12'>
+          <Image
+            src="/feature1.png"
+            alt='features1'
+            width={400}
+            height={400}
+            />
+          
+          <div className="text-left leading-tight">
+            <h3 className="text-2xl font-bold">
+              Make Your Own Interview
+            </h3>
+            <p className="text-lg font-normal">
+              You can choose any topic you want to practice text text text text text text text text text
+            </p>
+          </div>
+        </div>
+
+        <div className='flex flex-row justify-between space-x-8 mt-8'>
+          <div className="text-left leading-tight">
+            <h3 className="text-2xl font-bold">
+              Get Instant Feedback <br />By Your AI Agent
+            </h3>
+            <p className="text-lg font-normal">
+              You can choose any topic you want to practice text text text text text text text text text
+            </p>
+          </div>
+
+          <Image
+            src="/feature1.png"
+            alt='features1'
+            width={400}
+            height={400}
+          />
+        </div>
+
+        <div className='flex flex-row justify-between space-x-8 mt-8'>
+          <Image
+            src="/feature1.png"
+            alt='features1'
+            width={400}
+            height={400}
+          />
+
+          <div className="text-left leading-tight">
+            <h3 className="text-2xl font-bold">
+              Keep Track of Your Progress
+            </h3>
+            <p className="text-lg font-normal">
+              You can save your feedback and cheack it whenever you want text text text text text text text text text
+            </p>
+          </div>
+        </div>
+
+      </section>        
+
+      {/* <section></section> how to use */}
 
     </main>
   );
